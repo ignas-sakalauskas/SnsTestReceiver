bdabfe84d614b9b1ff1c1893a25a607517bf9e5f
Ignas <ignas-sakalauskas@users.noreply.github.com>
2024-04-22 21:56:48 +0100

    add v7

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ docker build . --file src/SnsTestReceiver.Api/Dockerfile --tag ignassakalauskas/
 # multi-platform (hardcoded tag)
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx use --builder mybuilder
-docker buildx build --platform linux/amd64,linux/arm64 --tag ignassakalauskas/sns-test-receiver:3.0-beta1 . --file ./src/SnsTestReceiver.Api/Dockerfile --no-cache --progress=plain --push
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm64/v7 --tag ignassakalauskas/sns-test-receiver:3.0-beta3 . --file ./src/SnsTestReceiver.Api/Dockerfile --no-cache --progress=plain --push
 
 docker buildx build --load --tag ignassakalauskas/sns-test-receiver:3.0-beta1 . --file ./src/SnsTestReceiver.Api/Dockerfile
